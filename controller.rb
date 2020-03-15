fe5647f0dd5676ef9c71ae0e3100f9378ab18635
Ariane Holford <aribomop@gmail.com>
2020-03-15 14:17:51 +0000

    add edit dog details form

--- controller.rb
+++ controller.rb
@@ -29,8 +29,36 @@ get '/dogs/:id' do
   erb(:show_dog)
 end
 
+#dog edit form
+get '/dogs/:id/edit' do
+  @dog = Dog.find(params['id'])
+  @no_owner = Owner.all.first.id.to_i
+  erb(:edit_dog)
+end
+
+#update dog details
+post '/dogs/:id' do
+  dog = Dog.new(params)
+  dog.update
+  redirect to "/dogs/#{params['id']}"
+end
+
+#delete a dog
+post '/dogs/:id/delete' do
+  dog = Dog.find(params['id'])
+  dog.delete
+  redirect to "/dogs"
+end
+
+#show all owners
 get '/owners' do
   owners = Owner.all
   @owners = owners.drop(1)
   erb(:index_owners)
+end
+
+#add a new owner
+get '/owners/new' do
+  @owners = Owner.all
+  erb(:new_owner)
 end
