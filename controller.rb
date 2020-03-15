3c4395d131ba8e91cd5b6cb6e1cf7cc69eb48ea6
Ariane Holford <aribomop@gmail.com>
2020-03-15 14:53:46 +0000

    add owner new, edit and show views

--- controller.rb
+++ controller.rb
@@ -62,3 +62,35 @@ get '/owners/new' do
   @owners = Owner.all
   erb(:new_owner)
 end
+
+#update the owners table
+post '/owners' do
+  Owner.new(params).save
+  redirect to '/owners'
+end
+
+#show owner profile
+get '/owners/:id' do
+  @owner = Owner.find(params['id'])
+  erb(:show_owner)
+end
+
+#owner edit form
+get '/owners/:id/edit' do
+  @owner = Owner.find(params['id'])
+  erb(:edit_owner)
+end
+
+#update owner details
+post '/owners/:id' do
+  owner = Owner.new(params)
+  owner.update
+  redirect to "/owners/#{params['id']}"
+end
+
+#delete an owner
+post '/owners/:id/delete' do
+  owner = Owner.find(params['id'])
+  owner.delete
+  redirect to "/owners"
+end
