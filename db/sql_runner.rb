df025128e717351f373427287856235409c92963
Ariane Holford <aribomop@gmail.com>
2020-06-30 11:19:48 +0100

    restyled owner page

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,9 +4,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect( {dbname: 'deq6t5eisnk40r',
-      host: 'ec2-52-86-73-86.compute-1.amazonaws.com',
-      port: 5432, user: 'ezmacprfgeutxu', password: '79694ba0bb36c40633e0856ddd10e232f80539e9e24a205b7fabd7e117bd1590'})
+      db = PG.connect({dbname: 'animal_shelter', host: 'localhost'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
